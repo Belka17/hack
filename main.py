5baf9293e0ea3a7e1f7cb382f535e40017531899
Semen Chudakov <semen.chudakov7@gmail.com>
2019-05-04 12:29:44 +0300

    refactor

--- main.py
+++ main.py
@@ -1,101 +1,191 @@
+import os
+
 import cv2
 import numpy as np
 import pytesseract
 from PIL import Image
+import platform
 
-# Path of working folder on Disk
-src_path = "D:/Labas/hack/"
-imageName = "7cr.png"
-def get_string(img_path):
+win = 'windows'
+linux = 'linux'
 
-    img = cv2.imread(img_path)
+plat = platform.system()
+
+src_path_linux = '/home/semen/drive/workspace.python/hack'
+src_path_win = 'D:\Labas\hack\crop'
+
+cropped_suffix = '_cropped'
+sharp_suffix = '_sharp'
+thres_suffix = '_thres'
+
+crop_dir_linux = '/home/semen/drive/workspace.python/hack/photos/crop/'
+orig_dir_linux = '/home/semen/drive/workspace.python/hack/photos/orig/'
+sharp_dir_linux = '/home/semen/drive/workspace.python/hack/photos/sharp/'
+thres_dir_linux = '/home/semen/drive/workspace.python/hack/photos/thres/'
+
+crop_dir_win = "D:\Labas\hack\crop"
+orig_dir_win = 'D:\Labas\hack\orig'
+sharp_dir_win = 'D:\Labas\hack\sharp'
+thres_dir_win = 'D:\Labas\hack\\thres'
+
+kernel_sharpening = np.array([[-1, -1, -1],
+                              [-1, 9, -1],
+                              [-1, -1, -1]])
+
+
+def get_origin_dir():
+    if plat == win:
+        return orig_dir_win
+    else:
+        return orig_dir_linux
+
+
+def get_crop_dir():
+    if plat == win:
+        return crop_dir_win
+    else:
+        return crop_dir_linux
+
+
+def get_sharp_dir():
+    if plat == win:
+        return sharp_dir_win
+    else:
+        return sharp_dir_linux
+
+
+def get_thres_dir():
+    if plat == win:
+        return thres_dir_win
+    else:
+        return thres_dir_linux
+
+
+def append_suffix(path: str, suffix: str) -> str:
+    base_name = os.path.basename(path)
+    name, extension = os.path.splitext(base_name)
+    return name + suffix + extension
+
+
+def get_croped_path(file_name: str) -> str:
+    return os.path.join(get_crop_dir(), append_suffix(file_name, cropped_suffix))
+
+
+def get_origin_path(file_name: str) -> str:
+    return os.path.join(get_origin_dir(), append_suffix(file_name, ''))
+
+
+def get_sharped_path(file_name: str) -> str:
+    return os.path.join(get_sharp_dir(), append_suffix(file_name, sharp_suffix))
+
+
+def get_thres_path(file_name: str) -> str:
+    return os.path.join(get_thres_dir(), append_suffix(file_name, thres_suffix))
+
+
+def get_string(file_path):
+    image = cv2.imread(file_path)
 
     # Convert to gray
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     kernel = np.ones((1, 1), np.uint8)
-    img = cv2.dilate(img, kernel, iterations=1)
-    img = cv2.erode(img, kernel, iterations=1)
 
-    cv2.imwrite(src_path + "thres.png", img)
+    image = cv2.dilate(image, kernel, iterations=1)
+    image = cv2.erode(image, kernel, iterations=1)
 
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+    THRES_IMAGE_PATH = get_thres_path(file_path)
 
-    result = pytesseract.image_to_string(Image.open(src_path + "thres.png"),lang="ukr")
+    cv2.imwrite(THRES_IMAGE_PATH, image)
+
+    # pytesseract.pytesseract.tesseract_cmd = r'tesseract'
+
+    result = pytesseract.image_to_string(Image.open(THRES_IMAGE_PATH), lang="ukr")
 
     return result
 
-imgClean = cv2.imread(src_path + imageName)
 
-img = cv2.cvtColor(imgClean,cv2.COLOR_BGR2HLS)
-sensitivity = 80
-lower_white = np.array([0,255-sensitivity,0])
-upper_white = np.array([255,255,255])
+if __name__ == '__main__':
+    image_name = '4.png'
 
-mask = cv2.inRange(img, lower_white, upper_white)
+    # print(get_origin_path(get_origin_path(image_name)))
+    # print(get_sharped_path(get_origin_path(image_name)))
+    # print(get_thres_path(get_origin_path(image_name)))
+    # print(get_croped_path(get_origin_path(image_name)))
 
-kernel = np.ones((4,4),np.uint8)
-dilation = cv2.dilate(mask,kernel,iterations = 5)
+    origin_image = cv2.imread(get_origin_path(image_name))
 
-kernel = np.ones((4,4),np.uint8)
-dilation = cv2.erode(dilation,kernel,iterations = 4)
+    hls_image = cv2.cvtColor(origin_image, cv2.COLOR_BGR2HLS)
 
+    sensitivity = 80
 
-contours, hierarchy = cv2.findContours(dilation,cv2.CHAIN_APPROX_SIMPLE,cv2.RETR_CCOMP)
-refArea = 0
-objectFound = False
-MIN_OBJECT_AREA = 100*100
+    lower_white = np.array([0, 255 - sensitivity, 0])
+    upper_white = np.array([255, 255, 255])
 
-if len(hierarchy) > 0:
-    numPbjects = len(hierarchy)
-    if numPbjects > 1 :
-        print(numPbjects + "ERROR")
-    else :
-        index = 0
-        #print(hierarchy)
-        while index >= 0:
-            moment = cv2.moments(contours[index])
-            area = moment["m00"]
-            if area > MIN_OBJECT_AREA and area > refArea:
-                x = moment["m10"] / area
-                y = moment["m01"] / area
-                objectFound = True
-                refArea = area
-            else:
-                objectFound = False
+    color_range_mask = cv2.inRange(hls_image, lower_white, upper_white)
 
-            if objectFound == True:
-                #print(x )
-                #print(y)
-                #print(contours[index])
-                break
+    kernel = np.ones((4, 4), np.uint8)
+    dilatied_image = cv2.dilate(color_range_mask, kernel, iterations=5)
 
-            index = hierarchy[0][index][0]
-leftUpper = contours[index]
-x_s = [elem[0][0] for elem in contours[index]]
-x_min = min(x_s)
-x_max = max(x_s)
-y_s = [elem[0][1] for elem in contours[index]]
-y_min = min(y_s)
-y_max = max(y_s)
+    kernel = np.ones((4, 4), np.uint8)
+    dilatied_image = cv2.erode(dilatied_image, kernel, iterations=4)
 
-crop_img = imgClean[y_min:y_max, x_min:x_max]
-equ = cv2.equalizeHist(cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY))
-afterPreproccesing = "cropped.png"
-cv2.imwrite(src_path + afterPreproccesing, crop_img)
+    contours, hierarchy = cv2.findContours(dilatied_image, cv2.CHAIN_APPROX_SIMPLE, cv2.RETR_CCOMP)
+    max_found_area = 0
+    object_found = False
 
+    MIN_OBJECT_AREA = 100 * 100
 
+    if len(hierarchy) > 0:
+        numPbjects = len(hierarchy)
 
-kernel_sharpening = np.array([[-1,-1,-1],
-                              [-1, 9,-1],
-                              [-1,-1,-1]])
+        if numPbjects > 1:
+            print(numPbjects + "ERROR")
+        else:
+            index = 0
+            # print(hierarchy)
+            while index >= 0:
+                moment = cv2.moments(contours[index])
+                area = moment["m00"]
+                if area > MIN_OBJECT_AREA and area > max_found_area:
+                    x = moment["m10"] / area
+                    y = moment["m01"] / area
+                    object_found = True
+                    max_found_area = area
+                else:
+                    object_found = False
 
-sharpened = cv2.filter2D(cv2.imread(src_path + afterPreproccesing), -1, kernel_sharpening)
-afterSharpening = "sharp.png"
-cv2.imwrite(src_path + afterSharpening, sharpened)
+                if object_found == True:
+                    # print(x)
+                    # print(y)
+                    # print(contours[index])
+                    break
+                index = hierarchy[0][index][0]
 
-print (get_string(src_path + afterSharpening))
+    left_upper = contours[index]
 
-print ("------ Done -------")
+    x_s = [elem[0][0] for elem in contours[index]]
+    x_min = min(x_s)
+    x_max = max(x_s)
 
+    y_s = [elem[0][1] for elem in contours[index]]
+    y_min = min(y_s)
+    y_max = max(y_s)
+
+    CROPPED_IMAGE_PATH = get_croped_path(image_name)
+    SHARPED_IMAGE_PATH = get_sharped_path(image_name)
+
+    cropped_image = origin_image[y_min:y_max, x_min:x_max]
+    equalized_image = cv2.equalizeHist(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY))
+
+    cv2.imwrite(CROPPED_IMAGE_PATH, cropped_image)
+
+    sharpened = cv2.filter2D(cv2.imread(CROPPED_IMAGE_PATH), -1, kernel_sharpening)
+
+    cv2.imwrite(SHARPED_IMAGE_PATH, sharpened)
+
+    parsed_text = get_string(SHARPED_IMAGE_PATH)
+
+    print(parsed_text)
+
+    print("------ Done -------")
